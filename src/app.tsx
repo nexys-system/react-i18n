49ad1137e804641289b98aabeb5808cce28b8659
Johan <johan@macair2020>
2021-08-20 16:31:01 +0200

    add basename

--- src/app.tsx
+++ src/app.tsx
@@ -3,8 +3,10 @@ import React from 'react';
 import I18n from './lib/main';
 import I18nWrapper from './lib/Wrapper';
 
+export const basename: string = import.meta.env.SNOWPACK_PUBLIC_URL || '';
+
 const language = localStorage.getItem('LANG') || 'en';
-const langUrl = '/assets/' + language + '.json';
+const langUrl = basename + '/assets/' + language + '.json';
 
 const i18n = new I18n(language, langUrl);
 
