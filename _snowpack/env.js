2c7e3f3204f6e89cfc3e40145c84ef9cd3917935
johnb8005 <johnb8005@users.noreply.github.com>
2021-10-26 08:45:34 +0000

    deploy: d5c3811d71e7e9ed3a54bf30cd0f28c76d07c2de

--- _snowpack/env.js
+++ _snowpack/env.js
@@ -1,4 +1,4 @@
-export const SNOWPACK_PUBLIC_GIT_SHA = "5767257f115fd52c56c7a83c6fc1dadb15e63b5c";
+export const SNOWPACK_PUBLIC_GIT_SHA = "d5c3811d71e7e9ed3a54bf30cd0f28c76d07c2de";
 export const SNOWPACK_PUBLIC_URL = "/react-i18n";
 export const MODE = "production";
 export const NODE_ENV = "production";
