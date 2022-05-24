e92cdf99a89347bd071bc0b816edf7e19d260a90
johnb8005 <johnb8005@users.noreply.github.com>
2022-05-24 10:38:44 +0000

    deploy: d19e4a205e5fe59cc30698666c9615a9416a0766

--- _snowpack/env.js
+++ _snowpack/env.js
@@ -1,4 +1,4 @@
-export const SNOWPACK_PUBLIC_GIT_SHA = "eb8b4cc6ce9d07ff35b2fb5cf8c8254d501605d3";
+export const SNOWPACK_PUBLIC_GIT_SHA = "d19e4a205e5fe59cc30698666c9615a9416a0766";
 export const SNOWPACK_PUBLIC_URL = "/react-i18n";
 export const MODE = "production";
 export const NODE_ENV = "production";
--- _snowpack/pkg/react.js
+++ _snowpack/pkg/react.js
@@ -1,2 +1,2 @@
-import { r as react } from './common/index-ae389540.js';
-export { r as default } from './common/index-ae389540.js';
+import { r as react } from './common/index-aec1d0e9.js';
+export { r as default } from './common/index-aec1d0e9.js';
